f622bae1ed37d9f74bed02b17ed7da88cc38ac9b
theognis1002 <theognis1002@gmail.com>
2024-12-01 21:33:02 -0500

    Add gitignore

--- build/resources/main/db/migration/V2__Add_users_table.sql
+++
@@ -1,5 +0,0 @@
--- Insert new users into the users table
-INSERT INTO users (name, email, password, role_id, created_at, updated_at) VALUES 
-('John Doe', 'john.doe123@example.com', 'hashed_password_1', 1, NOW(), NOW()),
-('Jane Smith', 'jane.smith123@example.com', 'hashed_password_2', 1, NOW(), NOW()),
-('Admin User', 'admin123@example.com', 'hashed_password_3', 2, NOW(), NOW());
